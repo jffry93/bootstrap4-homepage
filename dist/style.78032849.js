22b93b177aed597ab94869fd821416aa741f1257
jffry93 <jay.zalischi@gmail.com>
2021-02-16 17:25:16 -0500

    changed images and colors redesigned theshoebox co

--- dist/style.78032849.js
+++ dist/style.78032849.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/matrix.jpg":[["matrix.75c584d3.jpg","img/matrix.jpg"],"img/matrix.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/carousel/shoe1.png":[["shoe1.88b6cc0a.png","img/carousel/shoe1.png"],"img/carousel/shoe1.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57719" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64312" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
